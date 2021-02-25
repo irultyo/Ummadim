89d5602a937f178d51823015c745cc06a1229d20
irultyo <irul.tyo.7@gmail.com>
2021-02-25 21:23:23 +0700

    Database (done) & seeder(wip)

---
+++ database/migrations/2021_02_17_080017_create_post_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->text('caption');
+            $table->text('photo')->nullable();
+            $table->text('video')->nullable();
+            $table->integer('group');
+            $table->integer('like')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post');
+    }
+}
